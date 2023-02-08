714a6e143dc3a38c80a9b06c51187a2b3c29f376
Vadym <vadym.h@develops.today>
2023-02-08 22:29:13 +0200

    updated gitignore file

--- backend/dist/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolvers = void 0;
-const mutations_1 = require("./mutations");
-const queries_1 = require("./queries");
-exports.resolvers = {
-    Query: queries_1.RootQuery,
-    Mutation: mutations_1.RootMutation,
-};
